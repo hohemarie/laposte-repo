e5c7bffedd5a8490965bda47fea9188bfd82f795
hacker <hacker20190387468675@gmail.com>
2019-07-09 19:00:39 +0200

    git new commit

--- page2.php
+++ page2.php
@@ -2,15 +2,10 @@
 <?php
 echo "<pre>nginx: Linux packages</pre>";
 echo "<pre>",shell_exec('sudo apt install curl gnupg2 ca-certificates lsb-release'),"</pre>";
-echo "<pre>",shell_exec('echo "deb http://nginx.org/packages/ubuntu `lsb_release -cs` nginx"'),"</pre>";
-echo "<pre>",shell_exec('sudo tee /etc/apt/sources.list.d/nginx.list
-'),"</pre>";
-echo "<pre>",shell_exec('echo "deb http://nginx.org/packages/mainline/ubuntu `lsb_release -cs` nginx"'),"</pre>";
-echo "<pre>",shell_exec('sudo tee /etc/apt/sources.list.d/nginx.list'),"</pre>";
+echo "<pre>",shell_exec('echo "deb http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" \
+    | sudo tee /etc/apt/sources.list.d/nginx.list'),"</pre>";
 echo "<pre>",shell_exec('curl -fsSL https://nginx.org/keys/nginx_signing.key | sudo apt-key add -'),"</pre>";
 echo "<pre>",shell_exec('sudo apt-key fingerprint ABF5BD827BD9BF62'),"</pre>";
 echo "<pre>",shell_exec('sudo apt update'),"</pre>";
 echo "<pre>",shell_exec('sudo apt install nginx'),"</pre>";
-
-
 ?>
