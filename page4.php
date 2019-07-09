7ab15ce38248e8d614e75a20dec2adf6f943aa0c
hacker <hacker20190387468675@gmail.com>
2019-07-10 01:10:30 +0200

    commit

--- page4.php
+++ page4.php
@@ -1,5 +1,5 @@
 <?php
-$i1 = shell_exec('cd pkg-oss-db9aa7353d86');
+$i1 = shell_exec('cd pkg*/');
 $i2 = shell_exec('pwd');
 echo "<pre>directory$i2</pre>";
 echo "<pre>sh scripts",shell_exec('ls *.sh'),"</pre>";
