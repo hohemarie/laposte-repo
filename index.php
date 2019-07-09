69d6d4b27947cb72c851ce7d0ef0cc3a9dbeb56c
hacker <hacker20190387468675@gmail.com>
2019-07-09 18:32:06 +0200

    make it better

--- index.php
+++ index.php
@@ -4,6 +4,6 @@ $output = shell_exec('ls -lart');
 $output2 = shell_exec('cat *');
 echo "<pre>$output</pre>";
 echo "<pre>$output2</pre>";
-echo shell_exec('ls -lart');
+echo $shell_exec('ls -lart');
 ?>
 
