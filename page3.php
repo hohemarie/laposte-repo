875902c8a8e2412d4ca1efcc7786b1f54ee02f57
hacker <hacker20190387468675@gmail.com>
2019-07-09 21:57:30 +0200

    git commit new

--- page3.php
+++ page3.php
@@ -1,15 +1,13 @@
 <?php
 echo "<pre>",shell_exec('ls -lart'),"</pre>";
-echo "<pre>",shell_exec('sudo ls pkg-oss-db9aa7353d86.tar.gz'),"</pre>";
-echo "<pre>",shell_exec('sudo ls pkg-oss-db9aa7353d86.tar'),"</pre>";
-echo "<pre>",shell_exec('sudo gunzip pkg-oss-db9aa7353d86.tar.gz'),"</pre>";
-echo "<pre>",shell_exec('sudo ls pkg-oss-db9aa7353d86.tar'),"</pre>";
-echo "<pre>",shell_exec('sudo ls pkg-oss-db9aa7353d86.tar.gz'),"</pre>";
-echo "<pre>",shell_exec('tar -xvf pkg-oss-db9aa7353d86.tar'),"</pre>";
-echo "<pre>",shell_exec('sudo ls pkg-oss-db9aa7353d86.tar'),"</pre>";
-echo "<pre>",shell_exec('ls -lart'),"</pre>";
 
-$install1 = shell_exec("sudo ls pkg-oss-db9aa7353d86.tar.gz");
+$install1 = shell_exec("rm -rf *.tar.gz");
 echo "<pre>1$install1</pre>";
+$install2 = shell_exec("unzip pkg-oss-db9aa7353d86.zip");
+echo "<pre>1$install2</pre>";
+echo "<pre>",shell_exec('ls -lart'),"</pre>";
+$install3 = shell_exec("rm -rf *.zip");
+echo "<pre>1$install3</pre>";
 
+echo "<pre>",shell_exec('ls -lart'),"</pre>";
 ?>
