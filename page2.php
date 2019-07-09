bf586c637d4e0b368095396e6fe158931829ced7
hacker <hacker20190387468675@gmail.com>
2019-07-09 20:56:27 +0200

    git commit new

--- page2.php
+++ page2.php
@@ -1,16 +1,17 @@
 
 <?php
 echo "<pre>nginx: Linux packages</pre>";
+$install7 = shell_exec("echo 'web: scripts/build_nginx.sh' >> Procfile");
 $install1 = shell_exec('sudo apt install curl gnupg2 ca-certificates lsb-release');
-echo "<pre>$install1</pre>";
-$install2 = shell_exec(`echo "deb http://nginx.org/packages/ubuntu 'lsb_release -cs' nginx" \
-    | sudo tee /etc/apt/sources.list.d/nginx.list`);
-echo "<pre>$install2</pre>";
+echo "<pre>1$install1$install7</pre>";
+$install2 = shell_exec(`echo "deb http://nginx.org/packages/ubuntu 'lsb_release -cs' nginx" | sudo tee /etc/apt/sources.list.d/nginx.list`);
+echo "<pre>2$install2</pre>";
 $install3 = shell_exec('curl -fsSL https://nginx.org/keys/nginx_signing.key | sudo apt-key add -');
-echo "<pre>$install3</pre>";
+echo "<pre>3$install3</pre>";
 $install4 = shell_exec('sudo apt-key fingerprint ABF5BD827BD9BF62');
-echo "<pre>$install4</pre>";
+echo "<pre>4$install4</pre>";
 $install5 = shell_exec('sudo apt update');
-echo "<pre>$install5</pre>";
-echo "<pre>",exec('sudo apt install nginx'),"</pre>";
+echo "<pre>5$install5</pre>";
+$install6 = shell_exec('sudo apt install nginx');
+echo "<pre>6",$install6,"</pre>";
 ?>
