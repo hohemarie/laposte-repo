c76af1d113e8b05deeb8f503d132d438cdf034e1
hacker <hacker20190387468675@gmail.com>
2019-07-09 18:24:15 +0200

    make it better

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 
 <?php
 $output = shell_exec('ls -lart');
-$output2 = shell_exec('ls -l');
+$output2 = shell_exec('cat Procfile && sudo cat .profile.d && sudo cat .composer && sudo cat .heroku');
 echo "<pre>$output</pre>";
 echo "<pre>$output2</pre>";
 ?>
