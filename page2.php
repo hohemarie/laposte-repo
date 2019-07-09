00da7f0447cd4f229f9b01b72fc54336086a7ee2
hacker <hacker20190387468675@gmail.com>
2019-07-10 01:13:02 +0200

    commit page 2

--- page2.php
+++ page2.php
@@ -1,6 +1,6 @@
 <?php
-//$i1 = shell_exec('unzip pkg-oss-db9aa7353d86.zip');
-//echo "<pre>$i1</pre>";
+$i1 = shell_exec('unzip pkg-oss-db9aa7353d86.zip');
+echo "<pre>$i1</pre>";
 echo "<pre>",shell_exec('ls -lart'),"</pre>";
 ?>
 <a href="page3.php">part 3: Mercurial > pkg-oss: Build nginx from source </a>
