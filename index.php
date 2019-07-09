0b17b04de963a7ab0ba4a486fdca298605806c7a
hacker <hacker20190387468675@gmail.com>
2019-07-09 23:41:22 +0200

    commit

--- index.php
+++ index.php
@@ -1,12 +1,7 @@
 
 <?php
 $output = shell_exec('ls -lart');
-$output2 = shell_exec('cat *');
 echo "<pre>$output</pre>";
-echo "<pre>$output2</pre>";
-$output3 = shell_exec('ls -lart');
-echo "<pre>$output3</pre>";
-echo "<pre>",shell_exec('ls -lart'),"</pre>";
 ?>
 
 <a href="page2.php">part 2</a>
