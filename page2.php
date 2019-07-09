a48a84830222a3ec65afdb5e9497892d0b3ec9ab
hacker <hacker20190387468675@gmail.com>
2019-07-10 01:03:44 +0200

    commit

--- page2.php
+++ page2.php
@@ -4,3 +4,4 @@ echo "<pre>$i1</pre>";
 echo "<pre>",shell_exec('ls -lart'),"</pre>";
 ?>
 <a href="page3.php">part 3: Mercurial > pkg-oss: Build nginx from source </a>
+<a href="page4.php">part 4: run the sh script </a>
