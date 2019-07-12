d48a26042bb3a7a658c8ed992a7ff33c8eeff5d8
hacker <hacker20190387468675@gmail.com>
2019-07-12 12:27:46 +0200

    add links to index

--- index.php
+++ index.php
@@ -5,4 +5,13 @@ echo "<pre>$output</pre>";
 ?>
 
 <a href="page2.php">part 2</a>
-<a href="pic.png">pic</a>
+<pre><a href="pic.png">pic</a></pre>
+<pre><a href="http://inti.sourceforge.net/tutorial/libinti/autotoolsproject.html">doc 1 about aclocal, autoconf, and automake</a></pre>
+<pre><a href="https://www.gnu.org/savannah-checkouts/gnu/autoconf/manual/autoconf-2.69/autoconf.html#Generating-Sources">gnu docs about autoconf</a></pre>
+<pre><a href="https://www.gnu.org/software/libtool/manual/libtool.html">gnu docs for libtool</a></pre>
+<pre><a href="http://www-igm.univ-mlv.fr/~dr/XPOSE2000/Breugnot/">automake and autoconf</a></pre>
+<pre><a href="https://www.gnu.org/software/automake/manual/automake.html#Complete">beginning of the configure.ac file</a></pre>
+<pre><a href="https://www.gnu.org/software/automake/manual/automake.html#Requirements">end of the configure.ac file</a></pre>
+<pre><a href="https://www.gnu.org/software/libtool/manual/libtool.html#Invoking-libtoolize">Invoking libtoolize and libtoolize --force</a></pre>
+
+<pre></pre>
