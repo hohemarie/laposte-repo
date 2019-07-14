3368909264a8af51b5038959cc4a43661a2f21fd
hacker <hacker20190387468675@gmail.com>
2019-07-14 19:49:26 +0200

    commit

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ $output = shell_exec('ls -lart');
 echo "<pre>$output</pre>";
 shell_exec('export HEROKU_API_KEY=356352cc-06a0-46ca-83e1-3fc6914e4649');
 shell_exec('heroku login');
+shell_exec('ssh -v git@heroku.com');
 $releasesoutput = shell_exec('heroku releases --app developer-test-123456958478956 -n 5');
 echo "<pre>$releasesoutput</pre>";
 echo "<pre>my 10 latest heroku releases in<a href='releases.php'>json</a> format</pre>";
