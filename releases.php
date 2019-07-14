06d47361d70c8bae634f167adc11f6fdbf826d12
hacker <hacker20190387468675@gmail.com>
2019-07-14 20:19:40 +0200

    commit

--- releases.php
+++ releases.php
@@ -1,8 +1,8 @@
 <?php
 echo "<pre><a href='index.php'>back to home page</a></pre>";
 echo "<pre>my 10 latest heroku releases</pre>";
-$authtoken = shell_exec('export HEROKU_API_KEY=356352cc-06a0-46ca-83e1-3fc6914e4649');
-$releases = shell_exec('heroku releases --json -n 10');
+$authtoken = shell_exec('export HEROKU_API_KEY=356352cc-06a0-46ca-83e1-3fc6914e4649 2>&1');
+$releases = shell_exec('heroku releases --json -n 10 2>&1');
 echo "<pre>$releases</pre>";
 echo "<pre><a href='index.php'>back to home page</a></pre>";
 ?>
